f01c72699099ae94b5fefac345f90d06abbd8a7f
Joel Dice <joel.dice@gmail.com>
2013-07-31 15:26:00 -0700

    omit call to u_init
    u_init calls uplug_init, which may call fopen, which breaks on iOS.
    According to the documentation in uinit.c, we don't need to call this
    function anyway, so I just commented it out.  If we later find that
    some platforms require it, we can ifdef it instead.

--- luni/src/main/native/libcore_icu_ICU.cpp
+++ luni/src/main/native/libcore_icu_ICU.cpp
@@ -813,8 +813,10 @@ register_libcore_icu_ICU(JNIEnv* e)
   udata_setFileAccess(UDATA_NO_FILES, &status);
   if (status != U_ZERO_ERROR) abort();
 
-  u_init(&status);
-  if (status != U_ZERO_ERROR) abort();
+  // This causes trouble on iOS when uplug_init calls fopen.  Is it
+  // needed for other platforms?  If so, we'll have to ifdef it.
+//   u_init(&status);
+//   if (status != U_ZERO_ERROR) abort();
 
   jniRegisterNativeMethods(e, "libcore/icu/ICU", gMethods, NELEM(gMethods));
 }
