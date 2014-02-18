3cdca10ff15ca0f240c23ed365bc488f71fc92ae
Ilya Mizus <bigfatbrowncat@gmail.com>
2014-02-18 19:25:54 +0300

    Some other function stubs (and implementations) added

--- luni/src/main/native/mingw-extensions.h
+++ luni/src/main/native/mingw-extensions.h
@@ -90,19 +90,195 @@ int mkdir(const char *pathname, mode_t mode)
 	return mkdir(pathname);
 }
 
-// mlock/munlock
+// mlock/munlock/mmap
+
+#define PROT_NONE       0
+#define PROT_READ       1
+#define PROT_WRITE      2
+#define PROT_EXEC       4
+
+#define MAP_FILE        0
+#define MAP_SHARED      1
+#define MAP_PRIVATE     2
+#define MAP_TYPE        0xf
+#define MAP_FIXED       0x10
+#define MAP_ANONYMOUS   0x20
+#define MAP_ANON        MAP_ANONYMOUS
+
+#define MAP_FAILED      ((void *)-1)
+
+/* Flags for msync. */
+#define MS_ASYNC        1
+#define MS_SYNC         2
+#define MS_INVALIDATE   4
+
+#ifndef FILE_MAP_EXECUTE
+#define FILE_MAP_EXECUTE    0x0020
+#endif /* FILE_MAP_EXECUTE */
+
+static int mmap_winapi_error_to_posix(const DWORD winError)
+{
+    if (winError == 0)
+        return 0;
+
+    // TODO implement conversion. Now it doesn't convert anything yet
+	return winError;
+}
+
+static DWORD mmap_page(const int prot)
+{
+    DWORD protect = 0;
+   
+    if (prot == PROT_NONE)
+        return protect;
+       
+    if ((prot & PROT_EXEC) != 0)
+    {
+        protect = ((prot & PROT_WRITE) != 0) ?
+                    PAGE_EXECUTE_READWRITE : PAGE_EXECUTE_READ;
+    }
+    else
+    {
+        protect = ((prot & PROT_WRITE) != 0) ?
+                    PAGE_READWRITE : PAGE_READONLY;
+    }
+   
+    return protect;
+}
+
+static DWORD mmap_file(const int prot)
+{
+    DWORD desiredAccess = 0;
+   
+    if (prot == PROT_NONE)
+        return desiredAccess;
+       
+    if ((prot & PROT_READ) != 0)
+        desiredAccess |= FILE_MAP_READ;
+    if ((prot & PROT_WRITE) != 0)
+        desiredAccess |= FILE_MAP_WRITE;
+    if ((prot & PROT_EXEC) != 0)
+        desiredAccess |= FILE_MAP_EXECUTE;
+   
+    return desiredAccess;
+}
+
+void* mmap(void *addr, size_t len, int prot, int flags, int fildes, off_t off)
+{
+    HANDLE fm, h;
+   
+    void* map = MAP_FAILED;
+
+    const DWORD dwFileOffsetLow = (sizeof(off_t) <= sizeof(DWORD)) ? (DWORD)off : (DWORD)(off & 0xFFFFFFFFL);
+    const DWORD dwFileOffsetHigh = (sizeof(off_t) <= sizeof(DWORD)) ? (DWORD)0 : (DWORD)((off >> 32) & 0xFFFFFFFFL);
+    
+	const DWORD protect = mmap_page(prot);
+    const DWORD desiredAccess = mmap_file(prot);
+
+    const off_t maxSize = off + (off_t)len;
+
+    const DWORD dwMaxSizeLow = (sizeof(off_t) <= sizeof(DWORD)) ? (DWORD)maxSize : (DWORD)(maxSize & 0xFFFFFFFFL);
+    const DWORD dwMaxSizeHigh = (sizeof(off_t) <= sizeof(DWORD)) ? (DWORD)0 : (DWORD)((maxSize >> 32) & 0xFFFFFFFFL);
+
+    errno = 0;
+   
+    if (len == 0 || (flags & MAP_FIXED) != 0 || prot == PROT_EXEC)	// Here we check for unsupported flags
+    {
+        errno = EINVAL;
+        return MAP_FAILED;
+    }
+   
+    h = ((flags & MAP_ANONYMOUS) == 0) ? (HANDLE)_get_osfhandle(fildes) : INVALID_HANDLE_VALUE;
+
+    if ((flags & MAP_ANONYMOUS) == 0 && h == INVALID_HANDLE_VALUE)
+    {
+        errno = EBADF;
+        return MAP_FAILED;
+    }
+
+    fm = CreateFileMapping(h, NULL, protect, dwMaxSizeHigh, dwMaxSizeLow, NULL);
+
+    if (fm == NULL)
+    {
+        errno = mmap_winapi_error_to_posix(GetLastError());
+        return MAP_FAILED;
+    }
+ 
+    map = MapViewOfFile(fm, desiredAccess, dwFileOffsetHigh, dwFileOffsetLow, len);
+
+    CloseHandle(fm);
+ 
+    if (map == NULL)
+    {
+        errno = mmap_winapi_error_to_posix(GetLastError());
+        return MAP_FAILED;
+    }
+
+    return map;
+}
+
+int munmap(void *addr, size_t len)
+{
+    if (UnmapViewOfFile(addr))
+        return 0;
+	else
+	{
+		errno =  mmap_winapi_error_to_posix(GetLastError());
+		return -1;
+	}
+}
+
+int mprotect(void *addr, size_t len, int prot)
+{
+    DWORD newProtect = mmap_page(prot);
+    DWORD oldProtect = 0;
+   
+    if (VirtualProtect(addr, len, newProtect, &oldProtect))
+        return 0;
+	else
+	{
+		errno =  mmap_winapi_error_to_posix(GetLastError());
+		return -1;
+	}
+}
+
+int msync(void *addr, size_t len, int flags)
+{
+    if (FlushViewOfFile(addr, len))
+        return 0;
+	else
+	{
+		errno =  mmap_winapi_error_to_posix(GetLastError());
+		return -1;
+	}
+}
 
 int mlock(const void *addr, size_t len)
 {
-	// TODO use VirtualLock here
-	errno = EPERM;
-	return -1;
+    if (VirtualLock((LPVOID)addr, len))
+        return 0;
+	else
+	{
+		errno =  mmap_winapi_error_to_posix(GetLastError());
+		return -1;
+	}
 }
 
 int munlock(const void *addr, size_t len)
 {
-	// TODO use VirtualUnlock here
-	errno = EPERM;
+    if (VirtualUnlock((LPVOID)addr, len)) 
+		return 0;
+	else
+	{
+		errno =  mmap_winapi_error_to_posix(GetLastError());
+		return -1;
+	}
+}
+
+// pipe
+int pipe(int* pipefd)
+{
+	// TODO Implement this using winapi _pipe function 
 	return -1;
 }
 
@@ -420,6 +596,22 @@ int sysconf(int name)
 		errno = EINVAL;
 		return -1;
 	}
+}
+
+// sockets
+
+typedef unsigned int nfds_t;
+
+struct pollfd {
+    SOCKET fd;
+    short  events;
+    short  revents;
+};
+
+int poll(struct pollfd *fds, nfds_t nfds, int timeout)
+{
+	errno = EFAULT;
+	return -1;
 }
 
 #endif
